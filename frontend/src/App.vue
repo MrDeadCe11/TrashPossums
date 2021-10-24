9a7a7afeabe5cdc18eb798109853e59ef8479b6c
MrDeadce11 <spazzaroth@gmail.com>
2021-10-24 13:21:56 -0500

    added some vue components

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Modal caption="Greek Possum" />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.possum {
+  max-width: 200px;
+  max-height: 200px;
+}
+</style>
---
+++ frontend/src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <img alt="modal image" src="../assets/GREEKPOSSUM.png" class="possum" />
+      <p>{{ caption }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["caption"],
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: black;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: "../assets/simpleBg.jpg";
+  width: 100%;
+  height: 100%;
+}
+p {
+  color: blanchedalmond;
+  font-style: normal;
+}
+</style>
