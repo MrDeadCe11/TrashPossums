7f3db9062a05c2a56a1190dd6c17975a8bdba060
MrDeadCe11 <spazzaroth@gmail.com>
2021-12-13 16:52:00 -0600

    moved close button on modal

--- trash-site/src/components/Modal.vue
+++ trash-site/src/components/Modal.vue
@@ -1,10 +1,10 @@
 <template>
   <div  class="backdrop w-96 m-auto mt-32 p-5 h-auto fixed top-0 left-0 right-0 bg-gray-light" @click.self="toggleModal">
-      <div class="grid-col-4">
-          <button class="col-end-4 flex"><font-awesome-icon style="filter:invert(100%)" @click="$emit('toggle', $event.target.value) " :icon="['fas', 'times-circle']" class="w-5"/></button>
+     
+          <button class="flex ml-80"><font-awesome-icon style="filter:invert(100%)" @click="$emit('toggle', $event.target.value) " :icon="['fas', 'times-circle']" class="w-5"/></button>
           <slot>No Slot Passed</slot>
 
-      </div>
+      
   </div>
 </template>
 
