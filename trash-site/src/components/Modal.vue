757b5ad6a686e0adafeafa050715fa320031f722
MrDeadCe11 <spazzaroth@gmail.com>
2021-12-13 23:55:25 -0600

    minor tweak to email box

--- trash-site/src/components/Modal.vue
+++ trash-site/src/components/Modal.vue
@@ -1,7 +1,8 @@
 <template>
-  <div  class="backdrop w-96 m-auto mt-32 p-5 h-auto fixed top-0 left-0 right-0 bg-gray-light" @click.self="toggleModal">
-     
-          <button class="flex ml-80"><font-awesome-icon style="filter:invert(100%)" @click="$emit('toggle', $event.target.value) " :icon="['fas', 'times-circle']" class="w-5"/></button>
+  <div  class="backdrop w-4/6 m-auto mt-32 p-5 h-auto fixed top-0 left-0 right-0 bg-gray-light" @click.self="toggleModal">
+  <button class="col-span-6 justify-self-end -mr-10 -mt-5">
+<font-awesome-icon style="filter:invert(100%)" @click="$emit('toggle', $event.target.value) " :icon="['fas', 'times-circle']" />
+</button>
           <slot>No Slot Passed</slot>
 
       
--- trash-site/src/components/SubmitEmail.vue
+++ trash-site/src/components/SubmitEmail.vue
@@ -4,13 +4,12 @@
             <font-awesome-icon :icon="['fas', 'envelope']" /></button>
             </div>
              <div v-if="modal" class="fixed">        
-        <Modal class="border-4" @toggle="toggleModal"><slot>
-            <h1 class="text-center text-2xl text-white-light"> Join the Mailing list </h1>
-            <form action="">
-                <input placeholder="enter your email" required class="bg-gray-light border-2 pl-2 ml-5 mt-3">
-                <button class=" border-2 w-10 bg-gray-dark"><font-awesome-icon :icon="['fas', 'caret-left']" style="filter:invert(100%)" class="fill-white-light"/></button>
-
-            </form>
+        <Modal class="border-4 grid grid-rows-4 rid-cols-6 gap-3 justify-center" @toggle="toggleModal"><slot>
+            <h1 class="text-center col-span-6 text-2xl text-white-light"> Join the Mailing list </h1>
+            <form action="/emailList.php">
+            <input placeholder="enter your email" type="email" required class="bg-gray-dark border-2 pl-2 ml-20">
+            <button class="border-2 w-10 bg-gray-dark"><font-awesome-icon :icon="['fas', 'caret-left']" style="filter:invert(100%)" /></button>
+             </form>
              </slot> </Modal>
             </div>
       
