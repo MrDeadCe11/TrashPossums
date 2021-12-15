cf4afa88fc0d33e6c363bfb18bc407d1d4f192ac
MrDeadCe11 <spazzaroth@gmail.com>
2021-12-15 11:53:16 -0600

    more tweaks

--- trash-site/src/views/Team.vue
+++ trash-site/src/views/Team.vue
@@ -2,60 +2,60 @@
   <div class="team h-auto m-auto p-5 ml-5 mt-24 justify-content-center text-white-light">
      <h1 class="text-5xl">The Team</h1> 
     
-      <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+      <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl ">Stoney O'Possum</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light ">Stoney O'Possum</h1>           
+        <div class="float-right ml-6 -mt-7  sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/C2A35DF6-EFEF-4E0A-80D9-21552A94DD6D_pv8M62Aqeab.jpeg?updatedAt=1639521998737" alt="Stoney"/>
       </div>      
         <p class="ml-10" >Attaining oneness with life and toking across the continent to better understand who these "humans" were as a species. </p> 
      </div>       
     </div>
 
-     <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+     <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl ">Buskrat Slim</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light">Buskrat Slim</h1>           
+        <div class="float-right ml-6 -mt-7  sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/3BE34223-01DB-4DA8-8F68-C21ACE46D623_UpaAiBx6j9.jpeg?updatedAt=1639522000673" alt="Buskrat Slim">
       </div>      
         <p class="ml-10" >He is a possum.  he busks.  check out that lethal harmonica bandolier.</p> 
      </div>       
     </div>
 
-      <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+      <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl">Baron Von Parton</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light">Baron Von Parton</h1>           
+        <div class="float-right ml-6 -mt-7  sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/AC1D1B67-FC1D-4032-BCD8-20DB1F1148AE_MSHV_wL9pwq.jpeg?updatedAt=1639522000833" alt="Baron Von Parton">
       </div>      
         <p class="ml-10" >Baron Von Parton enjoys wearing hard clothing, and exploring the humans obsession with violence.</p> 
      </div>       
     </div>
 
-     <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+     <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl">JLD</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light">JLD</h1>           
+        <div class="float-right ml-6 -mt-7  sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/E5649160-1121-4CDD-8EB8-D2A2FB36FA10_ABvFzZupcSM.png?updatedAt=1639522052774" alt="JLD">
       </div>      
         <p class="ml-10" >He Draws stuff</p> 
      </div>       
     </div>
 
-     <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+     <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl">The SousaMaPoss</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light">The SousaMaPoss</h1>           
+        <div class="float-right ml-6 -mt-7  sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/75A0A23E-9865-4900-9B5B-E5D8735F66D9_Z2h3aifj2.jpeg?updatedAt=1639521995175" alt="The SousaMaPoss">
       </div>      
         <p class="ml-10" >Something something social media maybe?  something something.</p> 
      </div>       
     </div>
 
-      <div class="mt-4 mb-3 w-4/6 m-auto overflow-y-auto">
+      <div class="mb-3 w-4/6 m-auto overflow-y-auto bg-gray-light rounded p-2">
         <div class=" rounded-xl" style="background-postition: 10px 10px; margin-right: 8px;">
-        <h1 class="text-xl">Mr DeadCe11</h1>           
-        <div class="float-right ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+        <h1 class="text-xl w-full text-yellow-light">Mr DeadCe11</h1>           
+        <div class="float-right -mt-7 ml-6 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg" src="https://ik.imagekit.io/trashpossums/assets/614E777F-009C-4838-941D-DDCF93464B62_RRIXGzQtlXZ.jpeg?updatedAt=1639521995673" alt="Mr DeadCe11">
       </div>      
         <p class="ml-10" >Making sure all possum-kind is y2k compliant.</p> 
