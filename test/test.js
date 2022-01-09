01fefc24e0b84f560b74bea139d10746e5d21c08
MrDeadCe11 <spazzaroth@gmail.com>
2022-01-09 15:17:28 -0600

    update to tests

--- test/test.js
+++ test/test.js
@@ -25,23 +25,30 @@ describe("Trash Possums", function () {
     const deployed = await trashPossums.deployed();
     assert(deployed)
   });
+
+  it("should premint 100 possums", async function () {
+    const tx = await trashPossums.premintPossums();
+    const promise = await tx.wait();
+    const balance = await trashPossums.balanceOf(owner.address);
+assert(balance.toNumber() === 100)
+
+  })
   
   it("should get owner", async function() {
 const tx = await trashPossums.owner();
-
 console.log(tx);
     assert(tx === owner.address);
   })
 
-  it("should mint an nft to the owner address", async function () {
+  it("should mint an nft to the addr2 address", async function () {
     
-    const tx = await trashPossums.connect(owner).mintPossums(1);    
+    const tx = await trashPossums.connect(addr2).mintPossums(1);    
     const promise = await tx.wait();
     const event = promise.events.find(event => event.event === "Mint")
     const toAddress = event.args.mintedTo;
     tokenId1 = promise.events[0].args.tokenId.toNumber();
     console.log("minted to", toAddress, "Token Id", tokenId1)
-    assert(toAddress === owner.address);
+    assert(toAddress === addr2.address);
   });  
 
   it("should mint an nft to addr1", async function () {
@@ -57,22 +64,16 @@ console.log(tx);
     const ownerbal = await trashPossums.balanceOf(owner.address);
     const tx = await trashPossums.balanceOf(addr1.address);
     console.log("addr1 balance",tx.toNumber(), "owner balance", ownerbal.toNumber())
-    assert(tx.toNumber() === 1  && ownerbal.toNumber() === 1);
+    assert(tx.toNumber() === 1  && ownerbal.toNumber() === 100);
   });
 
-  it("should transfer first minted nft from owner to addr2", async function () {
-    const tx =await trashPossums.connect(owner)['safeTransferFrom(address,address,uint256)'](owner.address, addr1.address, tokenId1)
+  it("should transfer first minted nft from addr2 to addr1", async function () {
+    const tx =await trashPossums.connect(addr2)['safeTransferFrom(address,address,uint256)'](addr2.address, addr1.address, tokenId1)
     const promise = await tx.wait();   
     const event = promise.events.find(e=> e.event ==="Transfer")
     assert(event.args.to === addr1.address)
   })
-  it("should premint 100 possums", async function () {
-    const tx = await trashPossums.premintPossums();
-    const promise = await tx.wait();
-    const balance = await trashPossums.balanceOf(owner.address);
-assert(balance.toNumber() === 101)
 
-  })
   it("should return owner of token by id",  async function (){
     const poss1 = await trashPossums.ownerOf(tokenId1);
     const poss2 = await trashPossums.ownerOf(tokenId2);
