86bc0c5199434869928fe85c0d215b3982700c4a
MrDeadCe11 <spazzaroth@gmail.com>
2022-03-17 21:38:20 -0500

    website updates

--- trash-site/src/views/Purchase.vue
+++ trash-site/src/views/Purchase.vue
@@ -1,17 +1,22 @@
 <template>
-<div class="purchase mt-24">
-   <h1 class="font-bold content-evenly text-white-light text-2xl"> 
-     Purchase A Possum
-      <div
+<div class="purchase mt-24">      
+  <div class="w-4/6 m-auto bg-gray-light rounded p-2">
+    <div class="title sm:w-3/6 w-4/6 mb-3  m-auto text-clip">
+    <h1 class="font-bold content-evenly text-white-light sm:text-6xl text-xl"> 
+     Get you Some Possums
+     </h1>
+     </div>
+       <div
         @click="connect"
         
-        class="flex flex-row w-1/6 justify-center cursor-pointer items-center m-16 p-2 border-2 text-white-light border-blue-dark bg-blue-dark font-bold hover:text-white hover:bg-blue-light">
+        class="flex flex-row w-4/6 m-auto justify-center cursor-pointer items-center p-2 border-2 text-white-light border-blue-dark bg-blue-dark font-bold hover:text-white hover:bg-blue-light sm:">
         <img
             src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="MM"
             class="h-5 mr-2">
         Connect to Metamask
     </div>
-   </h1>
+    </div>
+   
 </div>
 </template>
 
@@ -19,15 +24,15 @@
 
 import BlockchainConnect from '../utils/getWeb3';
 
-
-
 export default {
   name:  'Purchase',
   components: {},
+  mounted() {
+  },
   setup() {
-    const connect = () => {
+  const connect = () => {
   const blockChain = new BlockchainConnect();
-  console.log("connect", blockChain)
+ 
   blockChain.connect();
     }
 
@@ -38,7 +43,6 @@ export default {
 
 <style>
 h1{
-  font-family: "Banco";
-  
+  font-family: "Banco";  
 }
 </style>
