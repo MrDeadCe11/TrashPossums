3353eb830b826408e1a123726f2c1684d2910b78
MrDeadCe11 <spazzaroth@gmail.com>
2022-03-30 13:29:08 -0500

    update to vite.config

--- trash-site/vite.config.js
+++ trash-site/vite.config.js
@@ -8,15 +8,16 @@ export default defineConfig({
   build: {
     chunkSizeWarningLimit:1500,
     rollupOptions: {
-      output:{
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-                return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-        }
+      // output:{
+      //     manualChunks(id) {
+      //       if (id.includes('node_modules')) {
+      //           return id.toString().split('node_modules/')[1].split('/')[0].toString();
+      //       }
+      //   }
       }
   }
   },
+
   resolve: {
   alias: {
     process: "process/browser",
