c0cf0029228fb417d536197eec2bd1ac61acf67b
MrDeadCe11 <mister.deadce11@gmail.com>
2022-06-24 18:42:46 -0500

    Update centerBox.vue

--- trash-site/src/components/centerBox.vue
+++ trash-site/src/components/centerBox.vue
@@ -2,13 +2,13 @@
   <div class="mb-3 md:w-4/6 w-full mx-auto overflow-y-auto bg-gray-light rounded p-2">
     <div class="rounded-xl mr-8">
       <h1 class="text-3xl ml-3 w-full text-yellow-light">{{ name }}</h1>
-      <div class="float-right ml-6 -mt-7 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
+      <div class="float-right ml-6 sm:-mt-7 sm:aspect-w-16 sm:aspect-h-9 sm:w-44">
         <img class="rounded-lg md:w-full w-2/3 m-auto" :src="image" alt="Member Profile pic" />
       </div>
       <p class="ml-10 text-2xl">{{ description }}</p>
     </div>
   </div>
-</template>
+</template> 
 
 <script>
 export default {
